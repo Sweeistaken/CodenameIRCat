a89a10705c3f5612ab668479284379f2de13e9c5
swee <meow@swee.codes>
2025-01-03 16:57:45 -0800

    Update server.py

--- server.py
+++ server.py
@@ -158,9 +158,8 @@ def session(connection, client, ip, ssl=False):
         if client[0] in banlist:
             print("Specified IP is banned, killing now.")
             reason = banlist[client[0]]
-            connection.sendall(bytes(f":{server} NOTICE * :{reason}\r\n","UTF-8"))
             connection.sendall(bytes(f":{server} 465 * :You are banned from this server\r\n","UTF-8"))
-            connection.sendall(bytes(f"ERROR :Closing Link: {hostname} (K-Lined)\r\n","UTF-8"))
+            connection.sendall(bytes(f"ERROR :Closing Link: {hostname} (K-Lined: {reason})\r\n","UTF-8"))
             time.sleep(3)
             raise Exception("Killed connection, IP is banned.")
         while True:
