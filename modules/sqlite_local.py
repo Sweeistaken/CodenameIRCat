0c6b493d247650cd882f0a7f745c894906926636
swee <meow@swee.codes>
2025-01-21 18:03:45 -0800

    Update modules/sqlite_local.py

--- modules/sqlite_local.py
+++ modules/sqlite_local.py
@@ -9,6 +9,7 @@ class broker:
             open(data_path, "w").write("")
         self.conn = sqlite3.connect(data_path)
         db = self.conn.cursor()
+        db.execute("""CREATE table IF NOT EXISTS bans (ip varchar(255), reason varchar(255)""")
         db.execute("""CREATE table IF NOT EXISTS nickserv (user varchar(255), modes varchar(255), hash varchar(255), cloak varchar(255))""")
         db.execute("""CREATE table IF NOT EXISTS groups (name varchar(255), owner varchar(255))""")
         db.execute("""CREATE table IF NOT EXISTS chanserv (name varchar(255), modes varchar(255), params varchar(255), owner varchar(255), usermodes varchar(255), optimodes varchar(255))""")
