44cbe23590948b9e81f0f35c8defeafacb5f4ad6
swee <meow@swee.codes>
2025-01-21 16:19:55 -0800

    Update modules/cfd1.py

--- modules/cfd1.py
+++ modules/cfd1.py
@@ -3,4 +3,10 @@ __ircat_type__ = "sql.provider" # The type of module
 __ircat_requires__ = ["cf_accountid", "cf_apitoken", "cf_d1database"] # The required config.yml entries.
 class broker:
     def __init__(self, cf_accountid, cf_apitoken, cf_d1database):
-        pass
+        self.account_id = cf_accountid
+        self.api_token = cf_apitoken
+        self.base_url = f"https://api.cloudflare.com/client/v4/accounts/{self.account_id}/d1/database"
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.api_token}"
+        }
