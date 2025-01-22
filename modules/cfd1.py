65e62656f544965c7a62bd6ec36bbc81b99a219d
swee <meow@swee.codes>
2025-01-21 16:16:51 -0800

    Add modules/cfd1.py

---
+++ modules/cfd1.py
@@ -0,0 +1,6 @@
+import requests, os
+__ircat_type__ = "sql.provider" # The type of module
+__ircat_requires__ = ["cf_accountid", "cf_apitoken", "cf_d1database"] # The required config.yml entries.
+class broker:
+    def __init__(self, cf_accountid, cf_apitoken, cf_d1database):
+        pass
