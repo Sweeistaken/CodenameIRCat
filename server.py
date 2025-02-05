a5d413f499fa1ed0245c0a08f5743c95ca81c394
swee <meow@swee.codes>
2025-02-05 15:40:01 -0800

    Update server.py

--- server.py
+++ server.py
@@ -306,7 +306,7 @@ def session(connection, client, ip, isssl=False):
             print("Received data: {}".format(data))
             try:
                 textt += data.decode()
-                if not property_list[pending]["ping_pending"]:
+                if pending != "*" and not property_list[pending]["ping_pending"]:
                     property_list[pending]["last_ping"] = time.time()
                 #if pending != "*":
                 #    if (time.time() - last_ping) > 30 and not ping_pending:
